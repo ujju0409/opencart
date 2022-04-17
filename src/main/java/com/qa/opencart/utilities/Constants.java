c67c5453828a081f421887e01da601a1a9dd3ad7
milanpatel9509 <milanpatel9509>
2022-04-17 09:36:29 -0400

    extend report change

--- src/main/java/com/qa/opencart/utilities/Constants.java
+++ src/main/java/com/qa/opencart/utilities/Constants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Constants {
 
-	 public static final String LOGIN_PAGE_TITLE = "11Account Login";
+	 public static final String LOGIN_PAGE_TITLE = "Account Login";
 	 public static final String LOGIN_PAGE_FRACTION_URL="route=account/login";
 	 public static final String ACCOUNT_PAGE_TITLE="My Account";
      public static final List<String> ACCOUNT_SECTION_LIST=
